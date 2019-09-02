d8f7a638f05b092c83d06fdbd65965adb89b4a8c
adam-dziedzic <adam.dziedzi@gmail.com>
2019-09-01 22:31:57 -0500

    change range of pixel values efficiently

--- simba_single.py
+++ simba_single.py
@@ -1,29 +1,30 @@
 import torch
-import utils
+import torch.nn.functional as F
+from cnns.nnlib.attacks.simple_blackbox_remote.torch_01_range import Ranger
 
-def normalize(x):
-    return utils.apply_normalization(x, 'imagenet')
+def normalize(x, dataset='imagenet'):
+    return Ranger(device=x.device).to_torch(x, dataset=dataset)
 
-def get_probs(model, x, y):
-    output = model(normalize(x.cuda())).cpu()
-    probs = torch.nn.Softmax()(output)[:, y]
-    return torch.diag(probs.data)
+def get_probs(model, x, dataset):
+    output = model(normalize(x.cuda(), dataset=dataset)).cpu()
+    return F.softmax(output, dim=-1).squeeze()
 
 # 20-line implementation of (untargeted) SimBA for single image input
-def simba_single(model, x, y, num_iters=10000, epsilon=0.2):
-    n_dims = x.view(1, -1).size(1)
+def simba_single(model, x, y, num_iters=10000, epsilon=0.2, dataset='imagenet'):
+    n_dims = x.numel()
     perm = torch.randperm(n_dims)
-    last_prob = get_probs(model, x, y)
+    last_probs = get_probs(model, x, dataset=dataset)
     for i in range(num_iters):
         diff = torch.zeros(n_dims)
         diff[perm[i]] = epsilon
-        left_prob = get_probs(model, (x - diff.view(x.size())).clamp(0, 1), y)
-        if left_prob < last_prob:
+        probs = get_probs(model, (x - diff.view(x.size())).clamp(0, 1), dataset=dataset)
+        if probs[y] < last_probs[y]:
             x = (x - diff.view(x.size())).clamp(0, 1)
-            last_prob = left_prob
         else:
-            right_prob = get_probs(model, (x + diff.view(x.size())).clamp(0, 1), y)
-            if right_prob < last_prob:
+            probs = get_probs(model, (x + diff.view(x.size())).clamp(0, 1), dataset=dataset)
+            if probs[y] < last_probs[y]:
                 x = (x + diff.view(x.size())).clamp(0, 1)
-                last_prob = right_prob
-    return x
+        if probs.argmax() != y:
+            return x
+        last_probs = probs
+    return None
